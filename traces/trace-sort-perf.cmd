8a739551bdc2484a818cb0f6f20b09cb772cb2dd
Cloud-Lee <cloud1027@gmail.com>
2022-02-22 01:15:18 -0800

    Add linux_qsort option for sort benchmark

---
+++ traces/trace-sort-perf.cmd
@@ -0,0 +1,30 @@
+# Test performance of sort compare to linux queue sort
+option fail 0
+option malloc 0
+new
+ih dolphin 1000000
+it gerbil 1000000
+time sort
+reverse
+time sort
+time sort
+reverse
+time sort
+new
+ih RAND 100000
+time sort
+ih RAND 100000
+time sort
+option linux_qsort 1
+new
+ih dolphin 1000000
+it gerbil 1000000
+time sort
+reverse
+time sort
+time sort
+new
+ih RAND 100000
+time sort
+ih RAND 100000
+time sort
